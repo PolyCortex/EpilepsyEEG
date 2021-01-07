1a371ffde85404a933eaf79c7ededd52fa6fb628
Remy Dumas <remy.dumas@polymtl.ca>
2021-01-07 18:02:11 -0500

    added MontageExtractor as a transformer (convert raw channels)

--- lib/transformers.py
+++ lib/transformers.py
@@ -5,9 +5,34 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.signal import spectrogram
 from utils import combiner
 
+class MontageExtractor(BaseEstimator, TransformerMixin):
+    """Takes raw data and extracted montages from channels"""
+    def __init__(self):
+        pass
+
+        self.fs = 0
+        self.montages = []
+        self.n_montages = 0
+        self.montage_type = 1
+
+    def fit(self,X):
+        """Reads metadata and set parameters according to the montage type"""
+        self.fs = X.info['sfreq']
+        self.montage_type = X.info['montage_type']
+        if self.montage_type == 3:
+            self.n_montages = 20
+        else:
+            self.n_montages = 22
+        return self
+
+    def transform(self,X):
+        self.montages, self.montages_names = combiner(X, montage_type=self.montage_type) # Make montages (difference of electrodes)
+        self.montages = [montage/montage.max() for montage in self.montages] # Normalization
+        self.montages = np.array(self.montages) # Conversion to numpy array
+        return self
 
 class SpectrogramTransformer(BaseEstimator, TransformerMixin):
-    """Takes raw data and returns the associated spectrogram"""
+    """Takes extracted montages (time-series data) and returns the associated spectrogram"""
     def __init__(self,
                 window=('tukey', .25),
 #                 nperseg=None,
@@ -31,22 +56,13 @@ class SpectrogramTransformer(BaseEstimator, TransformerMixin):
         self.win_duration = win_duration
 
     def fit(self, X):
-        """Reads metadata and set parameters according to the montage type"""
-        self.fs = X.info['sfreq']
-        self.nperseg = int(self.fs*self.win_duration)
-        self.montage_type = X.info['montage_type']
-        if self.montage_type == 3:
-            self.n_montages = 20
-        else:
-            self.n_montages = 22
+        self.nperseg = int(X.fs*self.win_duration)
         return self
 
     def transform(self, X):
         """Performs transformation. Computes for each montage the associated spectrogram. Returns also frequency and time range"""
-        montages, self.montages_names = combiner(X, montage_type=self.montage_type) # Make montages (difference of electrodes)
-        montages = [montage/montage.max() for montage in montages] # Normalization
-        spectrograms_list = [spectrogram(montages[id_montage],
-                                         fs=self.fs,
+        spectrograms_list = [spectrogram(X.montages[id_montage],
+                                         fs=X.fs,
                                          window=self.window,
                                          nperseg=self.nperseg,
                                          noverlap=self.noverlap,
@@ -56,7 +72,7 @@ class SpectrogramTransformer(BaseEstimator, TransformerMixin):
                                          scaling=self.scaling,
                                          axis=self.axis,
                                          mode=self.mode)
-                             for id_montage in range(self.n_montages)]
+                             for id_montage in range(X.n_montages)]
         self.f, self.t = spectrograms_list[0][:2] # f,t is the same for every montage
         spectrograms_db_list = [10*np.log10(Sxx) for (_,_,Sxx) in spectrograms_list]
         return self.t, self.f, spectrograms_db_list
